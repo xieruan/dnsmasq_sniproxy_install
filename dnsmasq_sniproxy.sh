be1da99c41ebcd336fc4734b0f96c66446b9273c
xieruan <59883140+xieruan@users.noreply.github.com>
2022-07-24 12:02:16 +0800

    Update dnsmasq_sniproxy.sh

--- dnsmasq_sniproxy.sh
+++ dnsmasq_sniproxy.sh
@@ -323,8 +323,8 @@ install_sniproxy(){
     elif check_sys packageManager apt; then
         if [[ ${fastmode} = "1" ]]; then
             if [[ ${bit} = "x86_64" ]]; then
-                download /tmp/sniproxy_0.6.0-2_amd64.deb http://ftp.de.debian.org/debian/pool/main/s/sniproxy/sniproxy_0.6.0-2_amd64.deb
-                dpkg -i --no-debsig /tmp/sniproxy_0.6.0-2_amd64.deb
+                wget -O /tmp/sniproxy_0.6.0-2_amd64.deb http://ftp.de.debian.org/debian/pool/main/s/sniproxy/sniproxy_0.6.0-2_amd64.deb
+                error_detect_depends "dpkg -i --no-debsig /tmp/sniproxy_0.6.0-2_amd64.deb"
                 rm -rf /tmp/sniproxy_0.6.0-2_amd64.deb
             elif [[ ${bit} = "i386" ]]; then
                 download /tmp/sniproxy_0.6.0_i386.deb https://github.com/myxuchangbin/dnsmasq_sniproxy_install/raw/master/sniproxy/sniproxy_0.6.0_i386.deb
